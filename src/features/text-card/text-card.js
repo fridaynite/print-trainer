4b262eb1577c9bdc745ec8a657d9c9f1931923f9
Леонид <getit7040@gmail.com>
2021-02-11 18:42:37 +0300

    initial structure

---
+++ src/features/text-card/text-card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Card from 'react-bootstrap/Card'
+
+function TextCard() {
+  return (
+    <Card>
+      <Card.Body>This is some text within a card body.</Card.Body>
+    </Card>
+  )
+}
+
+export default TextCard
